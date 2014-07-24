edd32e6efd4921d0e2fc03487529f9d3c80d69d5
Miroslav Genov <mgenov@gmail.com>
2014-07-24 22:05:44 +0300

    duplicated binding of TokenSecurity was being removed (I keep guicey binding, in case we would like to use more @Provides annotated methods instead of bind). This will make the transition faster.

--- src/main/java/com/example/auth/core/token/TokenSecurity.java
+++ src/main/java/com/example/auth/core/token/TokenSecurity.java
@@ -1,11 +1,9 @@
 package com.example.auth.core.token;
 
-import com.google.inject.ImplementedBy;
 
 /**
  * @author Ivan Stefanov <ivan.stefanov@clouway.com>
  */
-@ImplementedBy(TokenSecurityImpl.class)
 public interface TokenSecurity {
   Token create(TokenRequest request);
 }
